e49b786f784b5b6ac5873dfd5076811f6775cc49
xiaozhoushi <shixiaozhou0913@gmail.com>
2022-03-14 15:22:44 +0800

    init

---
+++ zsh/env.zsh
@@ -0,0 +1,10 @@
+export PATH=$PATH:$HOME/anaconda3/bin
+export CONFIG_HOME=$HOME/.config
+export LOCALBIN=$CONFIG_HOME/bin
+export PATH=$PATH:$LOCALBIN
+export PATH=$PATH:/Library/Tex/texbin
+#source $HOME/.zshenv
+
+echo -ne "\e[5 q"
+
+
